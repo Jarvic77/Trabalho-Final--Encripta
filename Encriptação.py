30e0a5e5566c9b8643ff1abe86f95bef63bae62f
Rodrigo Venâncio <89036718+rvenancio77@users.noreply.github.com>
2023-05-13 15:20:46 +0100

    Add files via upload
    Menu criado, funções separadas para criar e mostrar histograma e função para cifrar palavra função (encode)

--- Encriptação.py
+++ Encriptação.py
@@ -1,45 +1,88 @@
-frequencia = {}
+def encode(passe):
+    desloca = 3
+    alfabeto = "ABCDEFGHIJKLMANOPQRSTUVWXYZ"
+    passe_cifrada = ""
+    passe = passe.upper()
+    for letra in passe:
+        if letra in alfabeto:
+            indice = alfabeto.index(letra)
+            n_indice = (indice + desloca) % len(alfabeto)
+            passe_cifrada += alfabeto[n_indice]       
+        else:
+            passe_cifrada += letra
+    return passe_cifrada
 
-def encode(passe, desloca):
+def decode(passe):
     print("| ")
 
+def frequenciaHisto(texto):
+    frequencia = {} # Contar a frequência de cada letra do alfabeto
+    for letra in texto:
+        if letra in frequencia:
+            frequencia[letra] += 1 
+        else:
+            frequencia[letra] = 1
+    return frequencia
 
-def decode(passe, desloca):
-    print("| ")
+def mostraHistograma(texto):
+    for letra, freq in sorted(texto.items()): # percorre cada par chave-valor no dicionário frequencia. A função sorted() é usada para classificar as chaves do dicionário em ordem alfabética.
+        print(f"| {letra}: {'*' * freq}") #linha print(f"{letra}: {'*' * freq}") imprime uma mensagem na tela para cada letra e as vezes que repete
 
 
-with open('palavra-passe.txt', 'r') as f:
+#------DEFINIR AS VARIAVEIS GLOVAIS------
+with open('palavra-passe.txt', 'r') as f: #lê o ficheiro que tem a passe
     password = f.read()
+cifrada = encode(password)
+cifradaLower = cifrada.lower()
+cifradaLowerEspaço = ''.join(filter(str.isalpha, cifradaLower))
+passwordLower = password.lower() #converte a palavra-passe para minusculas
+passowrdLowerEspaço = ''.join(filter(str.isalpha, passwordLower)) # Remover todos os caracteres que não são letras
+#filter() é usada para verificar se é uma letra ou não. str.isalpha() verifica se é letra ou nao, se for é mantido. join() une os caracteres
+histogramaCifrada = frequenciaHisto(cifradaLowerEspaço)
+#------------
 
-passwordmin = password.lower() #converte a palavra-passe para minusculas
+def opcao(op):
+    if op == 1:
+        print("| ")
+        print("| --> Mostrar palavra-passe <--")
+        print("| Palavra passe: %s"%(password))
+        print("| ")
+        menu()
+    elif op == 2:
+        print("| ")
+        print("| --> Mostrar palavra-passe cifrada <--")
+        print("| Palavra passe cifrada: %s"%(cifrada))
+        print("| ")
+        menu()
+    elif op == 3:
+        print("| ")
+        print("| --> Mostrar histograma palavra-passe <--")
+        print("| Palavra passe: %s"%(password))
+        histogramaPassword = frequenciaHisto(passowrdLowerEspaço)
+        mostraHistograma(histogramaPassword)
+        print("")
+        menu()
+    elif op == 4:
+        print("| ")
+        print("| --> Mostrar histograma palavra-passe cifrada <--")
+        print("| Palavra passe cifrada: %s"%(cifrada))
+        histogramaCifrada = frequenciaHisto(cifradaLower)
+        mostraHistograma(histogramaCifrada)
+        menu()
+    elif op == 0:
+        exit()
 
-# A função filter() é usada para iterar sobre cada caractere do conteúdo do arquivo e verificar se ele é uma letra ou não. A função str.isalpha() é passada como 
-#argumento para filter() para testar se o caractere atual é uma letra. Se o caractere for uma letra, ele é mantido na sequência de caracteres filtrados.
-#Em seguida, a função join() é usada para unir todos os caracteres filtrados novamente em uma string. A string vazia '' é usada como separador entre os caracteres, 
-#o que significa que os caracteres filtrados são simplesmente concatenados sem nenhum espaço ou outro caractere de separação. 
 
-# Remover todos os caracteres que não são letras
-texto = ''.join(filter(str.isalpha, passwordmin))
+def menu():
+    print("| --------------------- MENU --------------------- ")
+    print("| 1 - Mostrar palavra-passe")
+    print("| 2 - Mostrar palavra-passe cifrada")
+    print("| 3 - Mostrar histograma palavra-passe")
+    print("| 4 - Mostrar histograma palavra-passe cifrada")
+    print("| 0 - Sair")
+    print("| ------------------------------------------------ ")
+    escolha = int(input("| Escolha uma opção: "))
+    print("| ------------------------------------------------ ")
+    opcao(escolha)
 
-# Contar a frequência de cada letra do alfabeto
-frequencia = {}
-for letra in texto:
-    if letra in frequencia:
-        frequencia[letra] += 1
-    else:
-        frequencia[letra] = 1
-
-print("| ")
-print ("| --> Palavra passe: %s"%(password))
-print("| ")
-
-#O loop for letra, freq in sorted(frequencia.items()): percorre cada par chave-valor no dicionário frequencia. A função sorted() 
-#é usada para classificar as chaves do dicionário em ordem alfabética.
-#------
-#A última linha print(f"{letra}: {'*' * freq}") imprime uma mensagem na tela para cada letra, que inclui a letra e
-#uma sequência de asteriscos * cujo comprimento é igual à frequência da letra. O resultado final é um histograma de 
-#letras com cada letra e o número de ocorrências representado por um número correspondente de asteriscos.
-print("| --> Histograma de Letras <--")
-for letra, freq in sorted(frequencia.items()):
-    print(f"| {letra}: {'*' * freq}")
-print("| ")
+menu()
