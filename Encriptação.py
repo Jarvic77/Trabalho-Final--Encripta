07f0c0d5c74611321e58c58e139542099d94b650
Rodrigo Venâncio <89036718+rvenancio77@users.noreply.github.com>
2023-05-28 22:58:02 +0100

    Add files via upload

--- Encriptação.py
+++ Encriptação.py
@@ -1,88 +1,141 @@
-def encode(passe):
-    desloca = 3
-    alfabeto = "ABCDEFGHIJKLMANOPQRSTUVWXYZ"
-    passe_cifrada = ""
-    passe = passe.upper()
-    for letra in passe:
-        if letra in alfabeto:
-            indice = alfabeto.index(letra)
-            n_indice = (indice + desloca) % len(alfabeto)
-            passe_cifrada += alfabeto[n_indice]       
+import string
+
+def encode(key, str):
+    encoded = ""
+    for i in range(len(str)):
+        char = str[i]
+        if char.isspace():
+            encoded += char
+        elif char.isupper():
+            encoded += chr((ord(char) + key - 65) % 26 + 65) #desloca o valor numérico ASCII do caractere pela chave e subtrai 65: %26 para garantir que o valor permaneça entre 0 e 25, e adiciona 65 para obter o valor numérico ASCII do caractere codificado.
         else:
-            passe_cifrada += letra
-    return passe_cifrada
+            encoded += chr((ord(char) + key - 97) % 26 + 97) #se nao for maiusculo
+    return encoded
 
-def decode(passe):
-    print("| ")
-
-def frequenciaHisto(texto):
-    frequencia = {} # Contar a frequência de cada letra do alfabeto
-    for letra in texto:
-        if letra in frequencia:
-            frequencia[letra] += 1 
+def decode(key, str):
+    decoded = ""
+    for i in range(len(str)):
+        char = str[i]
+        if char.isspace():
+            decoded += char
+        elif char.isupper():
+            decoded += chr((ord(char) - key - 65) % 26 + 65)
         else:
-            frequencia[letra] = 1
-    return frequencia
-
-def mostraHistograma(texto):
-    for letra, freq in sorted(texto.items()): # percorre cada par chave-valor no dicionário frequencia. A função sorted() é usada para classificar as chaves do dicionário em ordem alfabética.
-        print(f"| {letra}: {'*' * freq}") #linha print(f"{letra}: {'*' * freq}") imprime uma mensagem na tela para cada letra e as vezes que repete
+            decoded += chr((ord(char) - key - 97) % 26 + 97)
+    return decoded
 
 
-#------DEFINIR AS VARIAVEIS GLOVAIS------
-with open('palavra-passe.txt', 'r') as f: #lê o ficheiro que tem a passe
-    password = f.read()
-cifrada = encode(password)
-cifradaLower = cifrada.lower()
-cifradaLowerEspaço = ''.join(filter(str.isalpha, cifradaLower))
-passwordLower = password.lower() #converte a palavra-passe para minusculas
-passowrdLowerEspaço = ''.join(filter(str.isalpha, passwordLower)) # Remover todos os caracteres que não são letras
-#filter() é usada para verificar se é uma letra ou não. str.isalpha() verifica se é letra ou nao, se for é mantido. join() une os caracteres
-histogramaCifrada = frequenciaHisto(cifradaLowerEspaço)
-#------------
+def strencode(str):
+    string1 = "ZSBTRQLAMUWDVPNCXGEIFY"
+    string2 = "CBHDIGPXWJVQANETORYKZU"
+    encoded = ""
+    strup = str.upper()
+    for index in range(len(str)):
+        if strup[index].isspace():
+            encoded += strup[index]
+        elif index % 2 == 0:
+            for i in range(len(string.ascii_uppercase)):
+                if strup[index] == string.ascii_uppercase[i]:
+                    encoded += string1[i]
+        elif index % 2 != 0:
+            for i in range(len(string.ascii_uppercase)):
+                if strup[index] == string.ascii_uppercase[i]:
+                    encoded += string2[i]
+    return encoded
 
-def opcao(op):
-    if op == 1:
-        print("| ")
-        print("| --> Mostrar palavra-passe <--")
-        print("| Palavra passe: %s"%(password))
-        print("| ")
-        menu()
-    elif op == 2:
-        print("| ")
-        print("| --> Mostrar palavra-passe cifrada <--")
-        print("| Palavra passe cifrada: %s"%(cifrada))
-        print("| ")
-        menu()
-    elif op == 3:
-        print("| ")
-        print("| --> Mostrar histograma palavra-passe <--")
-        print("| Palavra passe: %s"%(password))
-        histogramaPassword = frequenciaHisto(passowrdLowerEspaço)
-        mostraHistograma(histogramaPassword)
-        print("")
-        menu()
-    elif op == 4:
-        print("| ")
-        print("| --> Mostrar histograma palavra-passe cifrada <--")
-        print("| Palavra passe cifrada: %s"%(cifrada))
-        histogramaCifrada = frequenciaHisto(cifradaLower)
-        mostraHistograma(histogramaCifrada)
-        menu()
-    elif op == 0:
-        exit()
+def strdecode(str):
+    string1 = "ZSBTRQLAMUWDVPNCXGEIFY"
+    string2 = "CBHDIGPXWJVQANETORYKZU"
+    decoded = ""
+    strup = str.upper()
+    for index in range(len(str)):
+        if strup[index].isspace():
+            decoded += strup[index]
+        elif index % 2 == 0:
+            for i in range(len(string.ascii_uppercase)):
+                if strup[index] == string1[i]:
+                    decoded += string.ascii_uppercase[i]
+        elif index % 2 != 0:
+            for i in range(len(string.ascii_uppercase)):
+                if strup[index] == string2[i]:
+                    decoded += string.ascii_uppercase[i]
+    return decoded
 
+def frequenciahistograma(str):
+    strlow = str.lower()
+    freq = {} # Contar a frequência de cada letra do alfabeto
+    for char in strlow:
+        if char in freq:
+            freq[char] += 1 
+        else:
+            freq[char] = 1
+    return freq
 
+def mostrahistograma(str):
+    for char, freq in sorted(str.items()): # percorre cada par chave-valor no dicionário frequencia. A função sorted() é usada para classificar as chaves do dicionário em ordem alfabética.
+        print(f"| {char}: {'*' * freq}") #linha print(f"{letra}: {'*' * freq}") imprime uma mensagem na tela para cada letra e as vezes que repete
+        
+#----- MENU -----
 def menu():
-    print("| --------------------- MENU --------------------- ")
-    print("| 1 - Mostrar palavra-passe")
-    print("| 2 - Mostrar palavra-passe cifrada")
-    print("| 3 - Mostrar histograma palavra-passe")
-    print("| 4 - Mostrar histograma palavra-passe cifrada")
+    print("|---------------------------------|") 
+    print("| 1 - Frase")
+    print("| 2 - Codificar")
+    print("| 3 - Codificar com 2 strings")
+    print("| 4 - Descodificar")
+    print("| 5 - Descodificar com 2 strings")
+    print("| 6 - Histograma normal")
+    print("| 7 - Histograma codificado")
+    print("| 8 - Histograma codificado com 2 strings")
+    print("| 9 - Novo deslocamento")
     print("| 0 - Sair")
-    print("| ------------------------------------------------ ")
-    escolha = int(input("| Escolha uma opção: "))
-    print("| ------------------------------------------------ ")
-    opcao(escolha)
+    print("| ")
+    op = int(input("| Introduza a opção: "))
+    print("|---------------------------------|") 
+    return op
 
-menu()
+#------DEFINIR AS VARIAVEIS GLOBAIS------
+with open('palavra-passe.txt', 'r') as f: #lê o ficheiro que tem a passe
+    text = f.read()
+deslocamento = (int(input("| Introduza quantas letras quer deslocar (1 - 25): ")))
+
+op = menu()
+while op != 0:
+    if op == 1:
+        print("| Frase: %s"%(text))  
+    elif op == 2:
+        encoded_text = encode(deslocamento, text)
+        print("| Codificada: %s"%(encoded_text))
+    elif op == 3:
+        encoded_strings = strencode(text)
+        print("| Codificada com 2 strings: %s"%(encoded_strings))
+    elif op == 4:
+        decoded_text = decode(deslocamento, encoded_text)
+        print("| Descodificada: ", decoded_text)
+    elif op == 5:
+        decoded_strings = strdecode(encoded_strings)
+        print("| Descodificada com 2 strings: ", decoded_strings)
+    elif op == 6:
+        text_nospace = ''.join(filter(str.isalpha, text)) # Remover todos os caracteres que não são letras
+        histograma_text = frequenciahistograma(text_nospace)
+        print("| ->Histograma frase<- ")
+        print("| Frase: %s"%(text))  
+        mostrahistograma(histograma_text)
+    elif op == 7:
+        encoded_nospace = ''.join(filter(str.isalpha, encoded_text))
+        histograma_encoded = frequenciahistograma(encoded_nospace)
+        print("| ->Histograma frase codificada<- ")
+        print("| Codificada: %s"%(encoded_text))
+        mostrahistograma(histograma_encoded)
+    elif op == 8:
+        encoded_strings_nospace= ''.join(filter(str.isalpha, encoded_strings)) # Remover todos os caracteres que não são letras
+        histograma_encoded_strins = frequenciahistograma(encoded_strings_nospace)
+        print("| ->Histograma codificado com 2 strings<- ")
+        print("| Codificada com 2 strings: %s"%(encoded_strings))
+        mostrahistograma(histograma_encoded_strins)
+    elif op == 9:
+        deslocamento = (int(input("| Introduza quantas letras quer deslocar (1 - 25): ")))
+    else:
+        print("| Opção inválida")
+    op = menu()
+
+
